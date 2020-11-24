a6d0b9168aef69a540f2a9651af2bbe8f97a721b
Nikriin <niklasnjohansen@gmail.com>
2020-11-24 19:40:07 +0100

    Gave Anzo Access to my pc - and he now fixed the uploader
     it works 100% unless you add a empty entry to the end of the file

--- uploader.php
+++ uploader.php
@@ -10,20 +10,25 @@ if(isset($_POST["submit"])) {
 		$file_as_string = file_get_contents($csv); //saves the contents of the CSV-file in a variable, that becomes a string
 		//print_r($file_as_string);
 		
-		$line_arr;  //an array of all the lines (that will eventually exist, just wait) in the uploaded CSV-file
-		$counter = 0;  //a counter for keeping track of index - will make more sense later
-		$line_arr = explode("\n",$file_as_string);  //creates an array of strings, where each string in the array is just one line from the CSV-file
 		
-		foreach($file_as_string as $entry)  //each of the newly created strings is saved under the variable name "entry" since it will be used as an entry in the database later on
+		$counter = 0;  //a counter for keeping track of index - will make more sense later
+		$temp_arr = explode(PHP_EOL,$file_as_string);  //creates an array of strings, where each string in the array is just one line from the CSV-file
+		$line_arr = array(count($temp_arr));  //an array of all the lines (that will eventually exist, just wait) in the uploaded CSV-file
+		$classCount = intval(count($temp_arr));
+		echo 'creating ' . intval($classCount-1) . " Classes<br>";
+		//var_dump($temp_arr);
+		//echo "<br>";
+		foreach($temp_arr as $entry)  //each of the newly created strings is saved under the variable name "entry" since it will be used as an entry in the database later on
 		{
-			if($couter != 0)
+			if($counter != 0)
 			{
 				$line = explode(",", $entry); //now we split each entry where a comma is present
 				$line_arr[$counter-1] = $line; //saves all the lines just made to an array. Uses counter as an index to cycle through the entire array
-				var_dump($line_arr[$counter]);
+				//var_dump($line_arr[$counter-1]);
 		
-				$counter++; //adds 1 to the counter, so that all the different indexes are used, and not just one of them
+				
 			}
+			$counter++; //adds 1 to the counter, so that all the different indexes are used, and not just one of them
 		}
 		$counter = 0;
 		/*
@@ -32,11 +37,11 @@ if(isset($_POST["submit"])) {
 		$mysql_internal->query($sql_req); //sends this to the database
 		$mysql_internal->close(); //closes the database, since we're done using it
 		*/
-		
+		//var_dump($line_arr);
 		foreach($line_arr as $row) //each of the lines in the array is saved to the "row" variable
 		{
 			$mysql_internal = Connect();  //connects to database
-			$sql_req = "INSERT INTO classes (className, studentCount) VALUES (" . "'" . $row[0] . "'". $row[1] . ")";  //stores the values from the new "row" array in the database
+			$sql_req = "INSERT INTO classes (className, studentCount) VALUES (" . "'" . $row[0] . "',". $row[1] . ")";  //stores the values from the new "row" array in the database
 			$mysql_internal->query($sql_req); //sends this to the database
 			$mysql_internal->close(); //closes the database, since we're done using it
 				
